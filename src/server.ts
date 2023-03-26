d4808a56a087c15218dcb8fb67f2538b4f148ecb
Jhonata <jhoownogueira@outlook.com>
2023-03-26 10:09:18 -0300

    ts node dev and debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express, {response} from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (resquest, response) => {
-    return response.json({message: 'Hello World!'});
+    return response.json({message: 'Hello!'});
+});
+
+app.post('/', (resquest, response) => {
+    const { name } = resquest.body;
+    return response.json({message: `O Curso é ${name}!`});
 })
 
-app.listen(3333);
+app.listen(3333, () => console.log('Server is running on port 3333'));
